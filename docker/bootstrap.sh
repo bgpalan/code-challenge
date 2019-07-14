41b42eb4de750bf024eb4cd4f66939bd04e75a39
Bharat Gopalan <>
2019-07-14 22:02:54 +0200

    Update bootstrap.sh
    Update the file, fix the issues and add comments

--- docker/bootstrap.sh
+++ docker/bootstrap.sh
@@ -1,6 +1,10 @@
-mkdir test && cd testecho "cloning the repo..."
+#!/bin/bash
+# Create a sample test directory
+mkdir test && cd test && echo "cloning the repo..."
+# clone the repo 
 git clone https://github.com/bgpalan/code-challenge.git
-cd code-challenge
-mvn clean install && cd target
+# Build the project 
+cd code-challenge && mvn clean install && cd target
+# Start the migration service jar 
 java -jar migration-service.jar
 
